ddbd84ca11965cc30b6e7d42a67cd457fd75afb2
Samuel Goldstein <99555794+samgoldst@users.noreply.github.com>
2022-10-26 17:10:22 -0700

    Update token_type.py

--- token_type.py
+++ token_type.py
@@ -3,6 +3,16 @@ import enum
 
 class TokenType(enum.Enum):
     ADD = enum.auto()
-    SUB = enum.auto()
+    EQU = enum.auto()
     REG = enum.auto()
     EOL = enum.auto()
+    COM = enum.auto()
+    EOF = enum.auto()
+    LDI = enum.auto()
+    OUT = enum.auto()
+    NUM = enum.auto()
+    JMP = enum.auto()
+    IFE = enum.auto()
+    IFL = enum.auto()
+    IFG = enum.auto()
+    COL = enum.auto()
